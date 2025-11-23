351cca1d2b30a45bff04d5ccc5cc5f22a95d9693
virtualheet <heetvavadiya456@gmail.com>
2025-11-23 18:23:00 +0530

    new Data Artist album card design set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen h-[200vh] flex flex-col justify-center text-xl items-center play bg-black">
+    <div className="w-full min-h-screen h-[200vh] flex flex-col justify-center text-2xl items-center  bg-black">
        <Feed/>
     </div>
   );
