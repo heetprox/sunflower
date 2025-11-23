ac73ff0c794d42215aa7183f77c611f75a404e31
virtualheet <heetvavadiya456@gmail.com>
2025-11-23 18:31:56 +0530

    layout part done

--- src/components/Album.tsx
+++ src/components/Album.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Album = ({ }) => {
     return (
         <div className=" flex flex-col w-full h-fit gap-2">
-            <div className="text-white mono font-normal tracking-tight text-2xl">25-5-2016</div>
+            <div className="text-white mono font-normal tracking-tight text-2xl">25-4-2015</div>
             <div className="flex gap-4 h-full w-full">
                 <div className="w-34 aspect-square bg-purple-400 rounded-lg"></div>
                 <div className="flex justify-between h-34 text-white play flex-col  gap-2 overflow-hidden   w-fit">
@@ -12,7 +12,7 @@ const Album = ({ }) => {
                         <div className="text-3xl font-bold">Light Switch</div>
                         <div className=" opacity-95 text-xl">Charlie puth</div>
                     </div>
-                    <div className="bg-green-400 items-center w-15 aspect-square rounded-full p-4">
+                    <div className="bg-purple-400 items-center w-15 aspect-square rounded-full p-4">
                         <div className="bg-black w-full h-full" style={{
                             clipPath: "polygon(80% 50%, 25% 90%, 25% 10%)"
                         }}></div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,26 +3,26 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className=' h-full   min-h-screen  p-4 border-white/50 bg-black flex flex-col w-fit gap-4'>
-        <div className="aspect-square items-center w-24 h-fit rounded-lg overflow-hidden">
+    <div className=' h-full   min-h-screen  p-4 border-white/50 bg-black flex flex-col w-28 overflow-hidden gap-4'>
+      <div className="aspect-square items-center w-full h-fit rounded-lg overflow-hidden">
 
-          <Image
-            src={"/like.webp"}
-            alt='like'
-            width={200}
-            height={200}
-            className='w-full h-full '
-          />
-        </div>
+        <Image
+          src={"/like.webp"}
+          alt='like'
+          width={200}
+          height={200}
+          className='w-full h-full '
+        />
+      </div>
 
-        <div className="aspect-square bg-green-400 
-        w-24 h-fit rounded-lg"></div>
+      <div className="aspect-square bg-green-400 
+        w-full h-fit rounded-lg"></div>
 
-        <div className="aspect-square w-24 bg-blue-400 h-fit rounded-lg"></div>
+      <div className="aspect-square w-full bg-blue-400 h-fit rounded-lg"></div>
 
-        <div className="aspect-square w-24 h-fit bg-yellow-300 rounded-lg"></div>
+      <div className="aspect-square w-full h-fit bg-yellow-300 rounded-lg"></div>
 
-        <div className="aspect-square bg-white w-24 h-fit rounded-lg"></div>
+      <div className="aspect-square bg-white w-full h-fit rounded-lg"></div>
 
     </div>
   )
