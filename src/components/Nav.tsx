a391a1bf426c6820da3d3ea9d7160326c451c2c5
virtualheet <heetvavadiya456@gmail.com>
2025-11-23 16:07:41 +0530

    added llike png

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Nav = () => {
   return (
     <div className=' h-full absolute left-0 min-h-screen  p-4 border-white/50 bg-black flex flex-col w-24 gap-5'>
-        <div className="aspect-square bg-white w-full h-fit rounded-lg"></div>
+        <div className="aspect-square items-center  bg-white w-full h-fit rounded-lg overflow-hidden">
+
+          <Image
+            src={"/like.webp"}
+            alt='like'
+            width={200}
+            height={200}
+            className='w-full h-full '
+          />
+        </div>
 
         <div className="aspect-square bg-white w-full h-fit rounded-lg"></div>
 
