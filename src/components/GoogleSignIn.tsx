134d6088f81df0ae352558dca71d3619a26a6edb
virtualheet <heetvavadiya456@gmail.com>
2026-02-01 18:02:40 +0530

    refactor(GoogleSignIn): rep

--- src/components/GoogleSignIn.tsx
+++ src/components/GoogleSignIn.tsx
@@ -1,20 +1,24 @@
 "use client";
 
+import { authClient } from "@/lib/auth-client";
+
 export function GoogleSignIn() {
-  const handleGoogleSignIn = () => {
-    const apiUrl =
-      process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
 
-    window.location.href = `${apiUrl}/auth/social/google?callbackURL=http://localhost:3000/dashboard&errorCallbackURL=http://localhost:3000/error`;
-  };
-
+const handleGoogleSignIn = async () => {
+  await authClient.signIn.social({
+    provider: "google",
+    callbackURL: "http://localhost:3000/dashboard",
+    errorCallbackURL: "http://localhost:3000/error"
+  });
+};
   return (
     <button
       onClick={handleGoogleSignIn}
       className=""
     >
-      <div className="rounded-lg edi text-md text-[#f93cb7] transition-all duration-100 hover:text-white underline underline-offset-4 cursor-pointer">
-        Sign in
+     
+      <div className="rounded-lg   edi  text-md  text-[#f93cb7] transition-all duration-100 hover:text-white underline underline-offset-4 cursor-pointer">
+      Sign in
       </div>
     </button>
   );
