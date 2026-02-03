d11580912c9a2429841b8b86dd6d15df9c178305
virtualheet <heetvavadiya456@gmail.com>
2026-02-03 13:09:44 +0530

    refactor: simplify next

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
+  /* config options here */
+  images:{
     unoptimized: true,
-  },
+  }
   
-  async rewrites() {
-    return [
-      {
-        source: '/api/auth/:path*',
-        destination: 'https://sunflower-backend-vv4o.onrender.com/api/auth/:path*',
-      },
-    ];
-  },
-  
-  // ✅ Add this to help debug
-  async headers() {
-    return [
-      {
-        source: '/api/auth/:path*',
-        headers: [
-          {
-            key: 'x-forwarded-host',
-            value: 'sunflower.realblue.lol',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
