e1d7a18852982264b3065aa6967acb8f9d5cd4ae
virtualheet <heetvavadiya456@gmail.com>
2026-02-03 12:37:59 +0530

    fix(auth): update auth client base URL

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images:{
+  images: {
     unoptimized: true,
-  }
+  },
+  
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: 'https://sunflower-backend-vv4o.onrender.com/api/auth/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
